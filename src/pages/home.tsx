261e0030d4a23d20da476497a6939b3dc61f1a3a
hafidnurkholis <itsmaehere@gmail.com>
2023-05-12 08:36:42 +0700

    chage hero image source

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -14,41 +14,25 @@ const HomePage: Component = () => {
         </h1>
       </header>
 
-      <img
-        src="https://images.prismic.io/canalstreetmarket/1d99cf0e3a0144c1229f92ee5b3c63b0eaae893c_home-hero.jpg?auto=compress,format"
-        alt=""
-        class="home-hero__image"
-      />
+      <div class="home-hero__image"></div>
 
       <section class="home-column">
         <h2 class="home-column__heading heading">A New Kind of Market</h2>
         <div class="three-grid__row">
           <div class="three-grid__row-item">
-            <img
-              src="https://images.prismic.io/canalstreetmarket/8f74dfabde9ebd66d0d078ba6cf794c77dc8ac5b_home_page_one.jpg?auto=compress,format"
-              alt=""
-              class="home-column__row-item-image"
-            />
+            <div class="home-column__row-item-image item-image-shop"></div>
             <p class="home-column__row-item-desc">
               Merging retail, food, art, and culture, Canal Street Market highlights top retail and design concepts, restaurants, and up-and-coming players in the downtown New York City community.
             </p>
           </div>
           <div class="three-grid__row-item">
-            <img
-              src="https://images.prismic.io/canalstreetmarket/20c7997073b456fda0e6de872d23b6183892bdde_home_page_two.jpg?auto=compress,format"
-              alt=""
-              class="home-column__row-item-image"
-            />
+            <div class="home-column__row-item-image item-image-retail"></div>
             <p class="home-column__row-item-desc">
               Retail Market Hours:<br />Fri– Sun: 11:00AM - 7:00PM
             </p>
           </div>
           <div class="three-grid__row-item">
-            <img
-              src="https://images.prismic.io/canalstreetmarket/13a988aff3e9c672350fe8330eb7386c3a85eeb9_home_page_three.jpg?auto=compress,format"
-              alt=""
-              class="home-column__row-item-image"
-            />
+            <div class="home-column__row-item-image item-image-food"></div>
             <p class="home-column__row-item-desc">
               Food Hall Hours:<br />
               Mon – Sun: 11:00AM - 8:00PM
