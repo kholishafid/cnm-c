88591989788a7b49cfc3c7a4117f3a2edccb63c5
hafidnurkholis <itsmaehere@gmail.com>
2023-05-16 15:06:38 +0700

    announcement component

--- src/components/announcement.tsx
+++ src/components/announcement.tsx
@@ -1,21 +1,25 @@
-import HappyIcon from "../icon-coponents/happy-icon";
+import '../assets/css/components/announcement.css'
+import { Component, JSXElement } from "solid-js";
 
-const AnnouncementSection = () => {
+const AnnouncementSection: Component<{ icon: JSXElement, children?: JSXElement }> = (props) => {
   return (
-
-    <section class="food-announcement">
-      <div>
-        <HappyIcon size={180} />
+    <section class="announcement">
+      <div class="announcement-icon">
+        {props.icon}
       </div>
-      <div>
-        <h1 class="heading food-announcement__col-heading">The Best of NYC</h1>
-        <p class="food-announcement__col-p">All under the roof!</p>
-      </div>
-      <div>
-        <HappyIcon size={180} />
+      {
+        props.children
+          ? props.children
+          : <div>
+            <h1 class="heading announcement__col-heading">The Best of NYC</h1>
+            <p class="announcement__col-p">All under the roof!</p>
+          </div>
+      }
+      <div class='announcement-icon announcement__icon-reverse'>
+        {props.icon}
       </div>
     </section>
   );
-}
+};
 
 export default AnnouncementSection;
